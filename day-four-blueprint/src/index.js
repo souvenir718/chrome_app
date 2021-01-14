730502b7ab3780cd1fa0031d63b8df5bd5d3e4e2
souvenir718 <mongkey0@naver.com>
2021-01-14 09:58:15 +0900

    day4 code Challenge outline

---
+++ day-four-blueprint/src/index.js
@@ -0,0 +1,9 @@
+// <⚠️ DONT DELETE THIS ⚠️>
+import './styles.css';
+// <⚠️ /DONT DELETE THIS ⚠️>
+
+function handleResize() {
+    console.log(window.innerWidth);
+}
+
+window.addEventListener('resize', handleResize);
