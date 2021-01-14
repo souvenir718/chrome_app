e5a38d733764b9bd9483444cb3e40ef531a841cc
souvenir718 <mongkey0@naver.com>
2021-01-14 10:26:49 +0900

    complete day4 code Challenge

--- day-four-blueprint/src/index.js
+++ day-four-blueprint/src/index.js
@@ -1,9 +1,19 @@
 // <⚠️ DONT DELETE THIS ⚠️>
+import { purple } from 'color-name';
 import './styles.css';
 // <⚠️ /DONT DELETE THIS ⚠️>
 
+const body = document.querySelector('body');
+
 function handleResize() {
     console.log(window.innerWidth);
+    if (window.innerWidth < 500) {
+        body.className = 'blue';
+    } else if (window.innerWidth >= 500 && window.innerWidth < 800) {
+        body.className = 'purple';
+    } else {
+        body.className = 'yellow';
+    }
 }
 
 window.addEventListener('resize', handleResize);
